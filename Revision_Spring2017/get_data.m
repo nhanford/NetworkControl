bf75f968f784accb68ea80d1b1f1670e7afb2ebd
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2017-01-15 23:37:27 -0800

    adding empty function

---
+++ Revision_Spring2017/get_data.m
@@ -0,0 +1,11 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Author: David Fridovich-Keil
+% File: get_data.m
+%
+% Function to find and read in data files.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function data = get_data(trial_dir, dest)
+
+end
