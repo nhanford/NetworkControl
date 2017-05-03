c7e723eec319d46f0b1504c38d450886ac5ab738
Nathan Hanford <nhanford@ucdavis.edu>
2017-05-03 01:47:32 -0700

    calling script

---
+++ Revision_Spring2017/python/localCaller.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for j in {1..5}
+do
+    python controllerTest.py .997 100 $i-pt1.es.net 1
+    sleep 5
+    mv localControlOutput.csv controlOutput-$j.csv
+    mv XI-0.997-PSI-100.0-iPerfOutput.csv iPerfOutput-$j.csv
+    rm *.bw
+    sleep 2
+done
+chown nate *.csv
+chmod +r *.csv 
+mkdir ~/`date +%F`
+mv *.csv ~/`date +%F`/
