b33ce7803bc357968c85cf4943b7fb8e014d84eb
Nathan Hanford <nhanford@ucdavis.edu>
2017-05-03 01:50:02 -0700

    calling script

--- Revision_Spring2017/python/localCaller.sh
+++ Revision_Spring2017/python/localCaller.sh
@@ -2,7 +2,7 @@
 
 for j in {1..5}
 do
-    python controllerTest.py .997 100 192.1668.1.1 1
+    python localControllerTest.py .997 100 192.168.1.1 1
     sleep 5
     mv localControlOutput.csv controlOutput-$j.csv
     mv XI-0.997-PSI-100.0-iPerfOutput.csv iPerfOutput-$j.csv
