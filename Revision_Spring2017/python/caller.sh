659f1e6067c8e05c95f8c60049d71381870f3876
Nathan Hanford <nhanford@ucdavis.edu>
2017-05-09 22:24:05 -0700

    pretest added to scripts

--- Revision_Spring2017/python/caller.sh
+++ Revision_Spring2017/python/caller.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+service irqbalance stop
+
+cpupower -c all frequency-set -g userspace
+cpupower -c all frequency-set -f 3.5G
+
+ethtool -G eth4 rx 2048 tx 1024
+ethtool -A eth4 rx off tx off
+ethtool -C eth4 rx-usecs 8 rx-frames 32 tx-usecs 8 tx-frames 8
+ethtool -K eth4 tso off gso off gro off tx-nocache-copy off
+
+sysctl -w net.ipv4.tcp_congestion_control=htcp
+
+tc qdisc del dev eth4 root
+
 for i in bost bois anl sacr
 do
     for j in {1..3}
--- Revision_Spring2017/python/localCaller.sh
+++ Revision_Spring2017/python/localCaller.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+service irqbalance stop
+
+cpupower -c all frequency-set -g userspace
+cpupower -c all frequency-set -f 3.5G
+
+ethtool -G eth4 rx 2048 tx 1024
+ethtool -A eth4 rx off tx off
+ethtool -C eth4 rx-usecs 8 rx-frames 32 tx-usecs 8 tx-frames 8
+ethtool -K eth4 tso off gso off gro off tx-nocache-copy off
+
+sysctl -w net.ipv4.tcp_congestion_control=htcp
+
+tc qdisc del dev eth4 root
+
 for j in {1..5}
 do
     python localControllerTest.py .997 100 192.168.1.1 $j
