e245446662146c33064e94a0dd840e134503c544
Nathan Hanford <nhanford@ucdavis.edu>
2017-05-25 15:06:04 -0700

    caller.sh now runs a competing injection flow

--- Revision_Spring2017/python/caller.sh
+++ Revision_Spring2017/python/caller.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
-service irqbalance stop
+sudo service irqbalance stop
 
-cpupower -c all frequency-set -g userspace
-cpupower -c all frequency-set -f 3.5G
+sudo cpupower -c all frequency-set -g userspace
+sudo cpupower -c all frequency-set -f 3.5G
 
-ethtool -G eth4 rx 2048 tx 1024
-ethtool -A eth4 rx off tx off
-ethtool -C eth4 rx-usecs 8 rx-frames 32 tx-usecs 8 tx-frames 8
-ethtool -K eth4 tso on gso on gro on tx-nocache-copy on
+sudo ethtool -G eth4 rx 2048 tx 1024
+sudo ethtool -A eth4 rx off tx off
+sudo ethtool -C eth4 rx-usecs 8 rx-frames 32 tx-usecs 8 tx-frames 8
+sudo ethtool -K eth4 tso on gso on gro on tx-nocache-copy on
 
-sysctl -w net.ipv4.tcp_congestion_control=htcp
+sudo sysctl -w net.ipv4.tcp_congestion_control=htcp
 
-tc qdisc del dev eth4 root
+sudo tc qdisc del dev eth4 root
 
 for i in bost bois anl sacr
 do
     for j in {1..3}
     do
-        python controllerTest.py 1 100 $i-pt1.es.net $j --on
-        tc qdisc del dev eth4 root
+        ssh nate@tilera "bwctl -T iperf3 -c $i-pt1.es.net -t60" &
+        sudo python controllerTest.py 1 100 $i-pt1.es.net $j --on
+        sudo tc qdisc del dev eth4 root
         sleep 2
-        rm *.bw
-        python controllerTest.py 1 100 $i-pt1.es.net $j
-        tc qdisc del dev eth4 root
-        rm *.bw
+        sudo rm *.bw
+        sudo python controllerTest.py 1 100 $i-pt1.es.net $j
+        sudo tc qdisc del dev eth4 root
+        sudo rm *.bw
         sleep 2
     done
 done
-chown nate *.csv
+sudo chown nate *.csv
 chmod +r *.csv 
 mkdir ~/`date +%F-%H-%M`
 mv *.csv ~/`date +%F-%H-%M`/
