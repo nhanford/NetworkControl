086d938a41d265a3abcaafb7e084e038287f270a
Nathan Hanford <nhanford@ucdavis.edu>
2017-05-30 12:28:05 -0700

    another sanity check script

---
+++ Revision_Spring2017/python/mtlb.py
@@ -0,0 +1,29 @@
+import os
+import json
+import argparse
+import csv
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Prepare iperf3 JSON output for MATLAB'
+    )
+    parser.add_argument(
+        'file', nargs='+', help='path to the JSON file'
+    )
+    args = parser.parse_args()
+    for path in args.file:
+        with open(path) as fp:
+            data = json.load(fp)
+        pth = os.path.splitext(path)
+        newpath = pth[0] + '.csv'
+        with open(newpath,'wb') as ofp:
+            writer=csv.writer(ofp)
+            title =  data['intervals'][0]['streams'][0].keys()
+            if 'omitted' in title: title.remove('omitted')
+            writer.writerow(title)
+            for interval in data['intervals']:
+                del(interval['streams'][0]['omitted'])
+                vals = interval['streams'][0].values()
+                writer.writerow(vals)
+if __name__ == '__main__':
+    main()
