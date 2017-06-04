15087360ac4afbb4b62309ce12e6903ea5e56fd2
Nathan Hanford <nhanford@ucdavis.edu>
2017-06-04 13:09:11 -0700

    Since maxrate makes a call to RTNETLINK, I'm making sure it's not also a culprit.

--- Revision_Spring2017/python/tcTest.sh
+++ Revision_Spring2017/python/tcTest.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-tc qdisc add dev eth4 root fq
+sudo tc qdisc add dev eth4 root fq
 while 1:
 do 
     sleep .01
     sudo tc qdisc change dev eth4 root fq maxrate 20Gbit
     sudo tc qdisc change dev eth4 root fq maxrate 21Gbit
 done
-tc qdisc del dev eth4 root
+sudo tc qdisc del dev eth4 root
