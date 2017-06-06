8ea7c230cc61170d1e1f4edaeec7e4d5ce759810
Nathan Hanford <nhanford@ucdavis.edu>
2017-06-06 02:03:03 -0700

    More changes to the sanity check scripts

--- Revision_Spring2017/python/tcTest.sh
+++ Revision_Spring2017/python/tcTest.sh
@@ -4,6 +4,7 @@ sudo tc qdisc add dev eth4 root fq
 while :
 do 
     sleep .01
+    ss -itn
     sudo tc qdisc change dev eth4 root fq maxrate 20Gbit
     sudo tc qdisc change dev eth4 root fq maxrate 21Gbit
 done
