1fe1d369669e91aac6c0582663ee8cd849649ccf
Nathan Hanford <nhanford@ucdavis.edu>
2017-06-15 01:52:05 -0700

    new script to change the egress rate every 10s

--- Revision_Spring2017/python/conCaller.sh
+++ Revision_Spring2017/python/conCaller.sh
@@ -4,8 +4,8 @@
 
 while :
 do
-    tc qdisc change dev eth3 root fq maxrate 5Gbit
+    sudo tc qdisc change dev eth3 root fq maxrate 5Gbit
     sleep 10
-    tc qdisc change dev eth3 root fq maxrate 10Gbit
+    sudo tc qdisc change dev eth3 root fq maxrate 10Gbit
     sleep 10
 done
