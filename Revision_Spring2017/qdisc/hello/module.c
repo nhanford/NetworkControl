d3925fe708dc004ebb7716ca79ce5cf7e2c40b1f
Taran Lynn <taranlynn0@gmail.com>
2018-06-23 13:00:11 -0700

    Changed module initialization and exit function names.

--- Revision_Spring2017/qdisc/hello/module.c
+++ Revision_Spring2017/qdisc/hello/module.c
@@ -9,7 +9,7 @@ MODULE_AUTHOR("Taran Lynn");
 MODULE_DESCRIPTION("A simple example Qdisc.");
 MODULE_VERSION("0.01");
 
-static int __init lkm_example_init(void) {
+static int __init hiqd_init(void) {
   register_qdisc(&hi_qdisc_ops);
   printk(KERN_INFO "Registered qdisc %s\n", hi_qdisc_ops.id);
   printk(KERN_INFO "Loaded module hiqd\n");
@@ -17,11 +17,11 @@ static int __init lkm_example_init(void) {
   return 0;
 }
 
-static void __exit lkm_example_exit(void) {
+static void __exit hiqd_exit(void) {
   unregister_qdisc(&hi_qdisc_ops);
   printk(KERN_INFO "Unregistered qdisc %s\n", hi_qdisc_ops.id);
   printk(KERN_INFO "Removed module hiqd\n");
 }
 
-module_init(lkm_example_init);
-module_exit(lkm_example_exit);
+module_init(hiqd_init);
+module_exit(hiqd_exit);
