b3ea01479cc95cca9cfc009bcf9c4d12f87704a1
Taran Lynn <taranlynn0@gmail.com>
2018-07-16 16:45:32 -0700

    Added a limit to dmesg backlog.

--- Revision_Spring2017/c_code/test/run.sh
+++ Revision_Spring2017/c_code/test/run.sh
@@ -3,8 +3,11 @@
 test=$1
 dest=$2
 
+maxlog=1000
+
+
 outputFiles=$(bwctl -c $dest -T iperf3 -i.1 -w150m -t60 --parsable -p)
 
 mv $outputFiles "$test-bwctl.json"
 
-dmesg | grep 'mpc: ' > "$test-dmesg.txt"
+dmesg | grep 'mpc: ' | tail -n $maxlog > "$test-dmesg.txt"
