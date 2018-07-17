ff620f3e0b2abed53cc90b04db898a5451303102
Taran Lynn <taranlynn0@gmail.com>
2018-07-16 19:21:46 -0700

    Added a helper script to plot all the data.

---
+++ Revision_Spring2017/c_code/test/plotAll.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for file in $(find . -name '*-bwctl.json')
+do
+  test=${file%-bwctl.json}
+
+  if [ -e $test-dmesg.txt ] && [ -e $test-etime.txt ]
+  then
+    python ./plot.py $test --title "Test: $test" --output $test-plot.png
+  fi
+done
