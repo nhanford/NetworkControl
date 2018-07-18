28117c8e83546e66943b5ddfebdd7d063ac8a122
Taran Lynn <taranlynn0@gmail.com>
2018-07-18 15:10:49 -0700

    Fixed spelling mistake.

--- Revision_Spring2017/c_code/test/run.sh
+++ Revision_Spring2017/c_code/test/run.sh
@@ -4,7 +4,7 @@ test=$1
 dest=$2
 
 # Output start and end times so we know where to look in dmesg.
-data +'%s' > "$test-time.txt"
+date +'%s' > "$test-time.txt"
 
 outputFiles=$(bwctl -c $dest -T iperf3 -i.1 -w150m -t60 --parsable -p)
 
