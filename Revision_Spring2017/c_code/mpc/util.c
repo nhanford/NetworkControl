38841ac6ed4564f53f1415b1155f4746f2dc8a25
Taran Lynn <taranlynn0@gmail.com>
2018-07-21 16:26:38 -0700

    I forgot to add the utility implementation file.

---
+++ Revision_Spring2017/c_code/mpc/util.c
@@ -0,0 +1,20 @@
+
+#include "util.h"
+
+void mpc_dfs_init(struct mpc_dfs_stats *dstats)
+{
+    dstats->root = debugfs_create_dir(MPC_DFS_DIR, NULL);
+
+    if(dstats->root == NULL) {
+        mpc_log("Failed to create dfs\n");
+    } else {
+        debugfs_create_u32("rtt_meas_us", 0644, dstats->root, &dstats->rtt_meas_us);
+        debugfs_create_u32("rate_set", 0644, dstats->root, &dstats->rate_set);
+        mpc_log("Created dfs\n");
+    }
+}
+
+void mpc_dfs_release(struct mpc_dfs_stats *dstats)
+{
+    debugfs_remove_recursive(dstats->root);
+}
