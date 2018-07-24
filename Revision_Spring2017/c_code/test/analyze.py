b19313a095d89a8244b8d842a8dad4d2149d1fdf
Taran Lynn <taranlynn0@gmail.com>
2018-07-23 19:19:39 -0700

    Added ability to match mean RTT to MPC ids.

--- Revision_Spring2017/c_code/test/analyze.py
+++ Revision_Spring2017/c_code/test/analyze.py
@@ -1,5 +1,9 @@
 
 import argparse
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 from data import Data
 
@@ -17,5 +21,10 @@ print(data.stream[['bits_per_second', 'rtt', 'rttvar', 'retransmits', 'snd_cwnd'
 print("\nFor Kernel Module")
 print(data.module[['rtt_meas_us', 'rate_set']].describe())
 
-print("MPC ids found")
-print(data.module.id.unique())
+idRTTMean = pd.DataFrame(columns=['id', 'mean_rtt_meas_us'])
+for x in data.module.groupby('id'):
+    idRTTMean = idRTTMean.append({'id': x[0],
+        'mean_rtt_meas_us': x[1].rtt_meas_us.mean()},
+        ignore_index = True)
+
+print(idRTTMean)
