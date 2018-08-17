1f479da791a3fca54f059333825c77c83ae6e0ee
Taran Lynn <taranlynn0@gmail.com>
2018-08-16 19:50:47 -0700

    Made the qdisc more efficient.
    The main changes are that flows are classified using a hash table, and
    flows sending packets are now kept in a linked list.

--- Revision_Spring2017/c_code/qdisc/sch_mpc.c
+++ Revision_Spring2017/c_code/qdisc/sch_mpc.c
@@ -32,7 +32,7 @@ struct mpc_flow {
 	bool used;
 
 	u32 addr;
-	struct mpc_flow *next;
+	struct list_head send_list;
 
 	struct sk_buff *head;
 	struct sk_buff *tail;
@@ -59,14 +59,17 @@ struct mpc_flow {
 
 
 struct mpc_sched_data {
-	// The flows. Currently index 0 is for unclassified flows, and is also the
-	// head of the flow chain.
+	// The flows. This is a hash map from destination addresses to flows.
 	struct mpc_flow *flows;
-	struct mpc_flow *tail;
-	struct mpc_flow *current_flow;
+
+	// Flow to use when classification fails or we are at our limit.
+	struct mpc_flow def_flow;
 
 	// Watchdog to set a timeout.
 	struct qdisc_watchdog watchdog;
+
+	// A list of flows that need to send packets.
+	struct list_head sending;
 };
 
 
@@ -75,7 +78,7 @@ static void flow_init(struct mpc_flow *flow)
 	flow->used = true;
 
 	flow->addr = 0;
-	flow->next = NULL;
+	INIT_LIST_HEAD(&flow->send_list);
 
 	flow->head = NULL;
 	flow->tail = NULL;
@@ -87,8 +90,8 @@ static void flow_init(struct mpc_flow *flow)
 	flow->last_srtt = 0;
 
 	flow->probing = false;
+	flow->target_lat = 0;
 	flow->probe_time_to_start = 0;
-	flow->probe_time_to_stop = 0;
 
 	model_init(&flow->md, 100, 10, 50, 50, 5, 1);
 }
@@ -136,20 +139,18 @@ static struct sk_buff *flow_dequeue(struct mpc_flow *flow)
 }
 
 
-static void flow_update_time_to_send(struct mpc_flow *flow, u64 time)
+static void flow_update_time_to_send(struct mpc_flow *flow, u64 now)
 {
 	u64 min_delay;
 
-	if (flow->head != NULL) {
-		if (flow->rate == 0)
-			min_delay = 0;
-		else
-			min_delay = NSEC_PER_SEC * flow->head->len / flow->rate;
+	if (flow->rate == 0)
+		min_delay = 0;
+	else
+		min_delay = NSEC_PER_SEC * flow->head->len / flow->rate;
 
-		// Take the max here to ensure that we don't go past the max rate on a
-		// burst.
-		flow->time_to_send = max_t(u64, time, flow->time_to_send + min_delay);
-	}
+	// Take the max here to ensure that we don't go past the max rate on a
+	// burst.
+	flow->time_to_send = max_t(u64, now, flow->time_to_send + min_delay);
 }
 
 static void flow_update_rate(struct mpc_flow *flow, u64 srtt_us, u64 time)
@@ -173,14 +174,14 @@ static void flow_update_rate(struct mpc_flow *flow, u64 srtt_us, u64 time)
 	flow->last_srtt = srtt_us;
 
 
-	if(!flow->probing && time >= flow->probe_time_to_start) {
+	if (!flow->probing && time >= flow->probe_time_to_start) {
 		flow->probing = true;
 		flow->probe_time_to_stop = time + MAX_PROBE_TIME_NS;
 		flow->target_lat = 2*rtt;
 	}
 
-	if(flow->probing) {
-		if(rtt >= flow->target_lat || time >= flow->probe_time_to_stop) {
+	if (flow->probing) {
+		if (rtt >= flow->target_lat || time >= flow->probe_time_to_stop) {
 			flow->probing = false;
 			flow->probe_time_to_start = time + INTER_PROBE_TIME_NS;
 		} else {
@@ -193,81 +194,111 @@ static void flow_update_rate(struct mpc_flow *flow, u64 srtt_us, u64 time)
 }
 
 
-// Returns the next flow that needs to send a packet at time. If not such flow
-// exists NULL is returned.
-static struct mpc_flow *mpc_get_next_flow(struct Qdisc *sch, u64 time)
-{
-	struct mpc_sched_data *q = qdisc_priv(sch);
-	struct mpc_flow *flow = q->current_flow;
-
-	while (flow != NULL) {
-		struct sk_buff *skb = flow_peek(flow);
-
-		if (flow->time_to_send <= time && skb != NULL) {
-			q->current_flow = flow;
-			return flow;
-		}
-
-		flow = flow->next;
-	}
-
-	flow = &q->flows[0];
-
-	while (flow != NULL && flow != q->current_flow) {
-		struct sk_buff *skb = flow_peek(flow);
-
-		if (flow->time_to_send <= time && skb != NULL) {
-			q->current_flow = flow;
-			return flow;
-		}
-
-		flow = flow->next;
-	}
-
-	return NULL;
-}
-
-
 // Returns the flow that skb is a part of. Creates a new flow if necessary.
 static struct mpc_flow *mpc_classify(struct Qdisc *sch, struct sk_buff *skb)
 {
-	// TODO: Reclaim flows. Also, I'm classifying by destination address. fq
-	// classifies by socket, and fq_codel uses a classiful qdisc (even though
-	// it isn't classful, *it's wierd*).
+	// NOTE: I'm classifying by destination address. fq classifies by
+	// socket, and fq_codel uses a classiful qdisc (even though it isn't
+	// classful, *it's wierd*).
 	struct mpc_sched_data *q = qdisc_priv(sch);
-	struct mpc_flow *unused_flow = NULL;
-	int i;
+	struct mpc_flow *flow = NULL;
+	size_t cnt = 0;
+	size_t i;
 
 	if (skb->sk == NULL)
 		goto class_default;
 
-	for (i = 1; i < NUM_FLOWS; i++) {
-		if (q->flows[i].used && q->flows[i].addr == skb->sk->sk_daddr)
-			return &q->flows[i];
-		else if (! q->flows[i].used)
-			unused_flow = &q->flows[i];
+	i = skb->sk->sk_daddr % NUM_FLOWS;
+
+	while (flow == NULL && cnt < NUM_FLOWS) {
+		if (!q->flows[i].used || q->flows[i].addr == skb->sk->sk_daddr) {
+			flow = &q->flows[i];
+		} else if (q->flows[i].qlen == 0) {
+			// Reclaim unused flows.
+			flow_release(&q->flows[i]);
+			flow = &q->flows[i];
+		} else {
+			i = (i + 1) % NUM_FLOWS;
+		}
+
+		cnt++;
 	}
 
-	if (unused_flow != NULL) {
-		flow_init(unused_flow);
-		unused_flow->addr = skb->sk->sk_daddr;
-		q->tail->next = unused_flow;
-		q->tail = unused_flow;
-		return unused_flow;
+	if (flow != NULL) {
+		if (!flow->used) {
+			flow_init(flow);
+			flow->addr = skb->sk->sk_daddr;
+		}
+
+		return flow;
 	}
 
 class_default:
-	// No unused flows, default to 0.
-	return &q->flows[0];
+	return &q->def_flow;
+}
+
+
+// Add a flow to the list of flows that have a packet to send.
+static void mpc_add_flow(struct Qdisc *sch, struct mpc_flow *flow)
+{
+	struct mpc_sched_data *q = qdisc_priv(sch);
+	struct mpc_flow *it;
+	bool add = true;
+
+	list_for_each_entry(it, &q->sending, send_list) {
+		if (it == flow)
+			add = false;
+	}
+
+	if (add)
+		list_add_tail(&flow->send_list, &q->sending);
+}
+
+
+// Add a flow to the list of flows that have a packet to send.
+static void mpc_del_flow(struct Qdisc *sch, struct mpc_flow *flow)
+{
+	list_del(&flow->send_list);
+}
+
+
+// Returns the next flow that needs to send a packet at time. If not such flow
+// exists NULL is returned.
+static struct mpc_flow *mpc_get_next_flow(struct Qdisc *sch)
+{
+	struct mpc_sched_data *q = qdisc_priv(sch);
+	struct mpc_flow *flow = NULL;
+	struct mpc_flow *it = NULL;
+	u64 tts = U64_MAX;
+
+	list_for_each_entry(it, &q->sending, send_list) {
+		if (it->time_to_send < tts) {
+			flow = it;
+			tts = it->time_to_send;
+		}
+	}
+
+	return flow;
+}
+
+
+// Update watchdog to dequeue when the next packet is ready.
+static void mpc_update_watchdog(struct Qdisc *sch, u64 now)
+{
+	struct mpc_sched_data *q = qdisc_priv(sch);
+	struct mpc_flow *flow = mpc_get_next_flow(sch);
+
+	if (flow != NULL)
+		qdisc_watchdog_schedule_ns(&q->watchdog, flow->time_to_send);
+	else
+		qdisc_watchdog_cancel(&q->watchdog);
 }
 
 
 static int mpc_enqueue(struct sk_buff *skb, struct Qdisc *sch,
 		struct sk_buff **to_free)
 {
-	struct mpc_sched_data *q = qdisc_priv(sch);
 	struct mpc_flow *flow = mpc_classify(sch, skb);
-	bool update_time = flow->head == NULL;
 
 	if (flow->qlen == sch->limit) {
 		return qdisc_drop(skb, sch, to_free);
@@ -275,9 +306,9 @@ static int mpc_enqueue(struct sk_buff *skb, struct Qdisc *sch,
 		u64 now = ktime_get_ns();
 
 		flow_enqueue(flow, skb);
-
-		if (update_time)
-			flow_update_time_to_send(flow, now);
+		flow_update_time_to_send(flow, now);
+		mpc_add_flow(sch, flow);
+		mpc_update_watchdog(sch, now);
 	}
 
 	return 0;
@@ -286,44 +317,34 @@ static int mpc_enqueue(struct sk_buff *skb, struct Qdisc *sch,
 
 static struct sk_buff *mpc_dequeue(struct Qdisc *sch)
 {
-	struct mpc_sched_data *q = qdisc_priv(sch);
 	struct mpc_flow *flow;
-	struct sk_buff *skb;
+	struct sk_buff *skb = NULL;
 	u64 now = ktime_get_ns();
-	u64 next_tts = 0;
-	size_t i;
 
-	flow = mpc_get_next_flow(sch, now);
+	flow = mpc_get_next_flow(sch);
 
-	if (flow == NULL) {
+	if (flow == NULL || flow->time_to_send > now) {
 		skb = NULL;
 		goto exit_dequeue;
 	}
 
 	skb = flow_dequeue(flow);
-	flow_update_time_to_send(flow, now);
+
+	if (flow->qlen == 0)
+		mpc_del_flow(sch, flow);
+	else
+		flow_update_time_to_send(flow, now);
 
 	// Update rate using MPC.
 	if (skb != NULL && skb->sk != NULL
 		&& skb->sk->sk_protocol == IPPROTO_TCP) {
-		// NOTE: There is a need to condsider this for different flows.
 		struct tcp_sock *tp = tcp_sk(skb->sk);
 
 		flow_update_rate(flow, tp->srtt_us, now);
 	}
 
 exit_dequeue:
-	for (i = 0; i < NUM_FLOWS; i++) {
-		if (next_tts == 0)
-			next_tts = q->flows[i].time_to_send;
-		else
-			next_tts = min_t(u64, next_tts, q->flows[i].time_to_send);
-	}
-
-	if (next_tts > 0)
-		qdisc_watchdog_schedule_ns(&q->watchdog, next_tts);
-	else
-		qdisc_watchdog_cancel(&q->watchdog);
+	mpc_update_watchdog(sch, now);
 
 	// Returns a packet to send out, NULL if we don't send out any.
 	return skb;
@@ -331,9 +352,12 @@ exit_dequeue:
 
 
 static struct sk_buff *mpc_peek(struct Qdisc *sch) {
-	struct mpc_sched_data *q = qdisc_priv(sch);
+	struct mpc_flow *flow = mpc_get_next_flow(sch);
 
-	return flow_peek(q->current_flow);
+	if (flow != NULL)
+		return flow_peek(flow);
+	else
+		return NULL;
 }
 
 
@@ -348,7 +372,6 @@ static int mpc_change(struct Qdisc *sch, struct nlattr *opt,
 		struct netlink_ext_ack *extack)
 #endif
 {
-	//struct mpc_sched_data *q = qdisc_priv(sch);
 	struct nlattr *tb[TCA_MPC_MAX + 1];
 	int err;
 
@@ -379,11 +402,10 @@ static int mpc_init(struct Qdisc *sch, struct nlattr *opt)
 	for (i = 0; i < NUM_FLOWS; i++)
 		q->flows[i].used = false;
 
-	flow_init(&q->flows[0]);
-	q->tail = &q->flows[0];
-	q->current_flow = q->tail;
+	flow_init(&q->def_flow);
 
 	qdisc_watchdog_init(&q->watchdog, sch);
+	INIT_LIST_HEAD(&q->sending);
 
 	sch->limit = qdisc_dev(sch)->tx_queue_len;
 
@@ -400,7 +422,6 @@ static int mpc_init(struct Qdisc *sch, struct nlattr *opt)
 
 static int mpc_dump(struct Qdisc *sch, struct sk_buff *skb)
 {
-	//struct mpc_sched_data *q = qdisc_priv(sch);
 	struct nlattr *opts;
 
 	opts = nla_nest_start(skb, TCA_OPTIONS);
